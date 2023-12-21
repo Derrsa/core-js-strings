0e186477780f3ef48ca8425d96c7d35aa766081a
Ivan <igrimasok@gmail.com>
2023-12-21 13:32:28 +0300

    feat: polindr task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -342,8 +342,9 @@ function countVowels(str) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const clearStr = str.toLowerCase().replace(/[\s.,!?]/g, '');
+  return clearStr === clearStr.split('').reverse().join('');
 }
 
 /**
