71abca8bfb06100b783516db86c54b33bae1a521
Ivan <igrimasok@gmail.com>
2023-12-21 13:06:21 +0300

    feat: 8-12 task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -178,8 +178,21 @@ function removeFirstOccurrences(str, value) {
  *   removeLastOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeLastOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeLastOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeLastOccurrences(str, value) {
+  if (typeof str !== 'string' || typeof value !== 'string') {
+    // Handle cases where either str or value is not a string
+    return str;
+  }
+
+  const index = str.lastIndexOf(value);
+
+  if (index !== -1) {
+    // If the substring is found, remove it and return the modified string
+    return str.slice(0, index) + str.slice(index + value.length);
+  }
+
+  // If the substring is not found, return the original string
+  return str;
 }
 
 /**
@@ -209,8 +222,8 @@ function sumOfCodes(/* str */) {
  *   startsWith('Hello World', 'World') => false
  *   startsWith('Hello World', 'Hello') => true
  */
-function startsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function startsWith(str, substr) {
+  return str.startsWith(substr);
 }
 
 /**
@@ -224,8 +237,8 @@ function startsWith(/* str, substr */) {
  *   endsWith('Hello World', 'World') => true
  *   endsWith('Hello World', 'Hello') => false
  */
-function endsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function endsWith(str, substr) {
+  return str.endsWith(substr);
 }
 
 /**
